544ba9d06ea74e62206684b43432851eae59d89c
Michael Berryman <berryman.michael@yahoo.com>
2019-11-01 10:57:19 -0700

    create basic branching in the JavaScript business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 //Business Logic
 var userOutput = function(userInput) {
-  return("1");
-}
+  if (userInput === 3) {
+    return ("I'm sorry, Dave. I'm afraid I can't do that.");
+  } else if (userInput === 2) {
+    return ("Boop!");
+  } else if (userInput === 1) {
+    return ("Beep!");
+  } else {
+    return (userInput);
+  }
+
+};
 
 //User Logic
 $(document).ready(function() {
